07385fc4d92fa38586d5842df51c327488842004
SardarBadar <dataentrybadar@gmail.com>
2022-09-05 12:01:46 +0500

    fix bug

--- page.php
+++ page.php
@@ -23,9 +23,14 @@ while (have_posts()) {
     <div class="metabox metabox--position-up metabox--with-home-link">
       <?php
       $parent_id=wp_get_post_parent_id(get_the_ID());
+      $parent=$parent_id;
+      $children= get_pages(array(
+        "child_of"=>get_the_ID()
+      ));
       if ( $parent_id=== 0) {
         $parent_id=get_the_ID();
-      } else {
+      } else{
+        
       ?>
         <p>
           <a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_id) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php  echo get_the_title($parent_id) ?></a> <span class="metabox__main"><?php the_title(); ?></span>
@@ -33,6 +38,8 @@ while (have_posts()) {
   
       <?php } ?>
     </div>
+    
+    <?php if($parent or $children){ ?>
     <!-- Side Bar menu -->
     <div class="page-links">
         <h2 class="page-links__title"><a href="<?php echo get_permalink($parent_id) ?>"><?php  echo get_the_title($parent_id) ?></a></h2>
@@ -45,16 +52,14 @@ while (have_posts()) {
             "title_li"=>null,
             "child_of"=>$parent_id
           ))?></a></li>
-          <?php  ?>
         </ul>
       </div>
+      <?php } ?>
 
     <div class="generic-content">
       <p><?php the_content() ?></p>
     </div>
   </div>
-
-
 <?php }
 
 get_footer();
