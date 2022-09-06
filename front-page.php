501d8bd309e1962a4faa56cf3c1e3b8a5652de46
SardarBadar <dataentrybadar@gmail.com>
2022-09-06 19:45:58 +0500

    minorchanges

--- front-page.php
+++ front-page.php
@@ -46,7 +46,7 @@
           </div>
         </div> -->
         
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
     </div>
