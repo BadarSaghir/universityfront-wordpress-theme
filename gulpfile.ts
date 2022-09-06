9d33b959b1af2b94e679ec0538308c79a21e2aaa
SardarBadar <dataentrybadar@gmail.com>
2022-09-06 13:23:49 +0500

    gulp setup

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,19 +1,35 @@
-import {watch,parallel, task} from "gulp";
+import {watch,parallel, task,src, dest, series} from "gulp";
 import  {create} from "browser-sync"
 import { TaskCallback } from "undertaker";
+// import del from 'del'
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 
+
+let browser_sync=create()
 function sync(cb: TaskCallback) {
-   let browser_sync=create()
+  
  browser_sync.init({
-    proxy:"smile-now.local",
-    files:"**/*",
-   
+    proxy:"smile-now.local",   
     });
-    watch("**/*").on('change',(c)=>{browser_sync.reload()})
+    watch("**/*\.(php|ts|js|css|html)").on('change',()=>{browser_sync.reload()})
     cb()
     
 }
-task('watch',(cb)=>sync(cb))
+const styles=() => {
+    return src('./scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('.'));;
+}
+
+// task('clean', () => {
+//     return del.deleteAsync([
+//         'css/main.css',
+//     ]);
+// });
+
+task('watch',parallel([(cb)=>{sync(cb)},(cb)=>{styles();watch("**/*\.(scss)").on('change',(cb)=>{styles()})}]))
 
 // export const build = parallel((cb)=>{sync(cb)})
 
