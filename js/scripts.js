e346c6cdfcc3174d1134e7f5f73b6b54d399bf75
SardarBadar <dataentrybadar@gmail.com>
2022-09-10 14:24:51 +0500

    fiexes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 // 3rd party packages from NPM
 import $ from 'jquery';
 import slick from 'slick-carousel';
-import Search from './modules/Search';
+
 // Our modules / classes
 import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
+import GoogleMap from './modules/GoogleMap';
+import Search from './modules/Search';
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
-var search = new Search()
+var googleMap = new GoogleMap();
+var search = new Search();
