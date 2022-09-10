e9fcdc0fbdf3ca9c766a4d6492bd8de572af6da0
SardarBadar <dataentrybadar@gmail.com>
2022-09-10 13:21:04 +0500

    added search

--- functions.php
+++ functions.php
@@ -21,7 +21,10 @@ function pageBanner()
 
 <?php
 }
-
+add_filter("acf/fields/google_map/api",function($api){
+$api['key']="AIzaSyBrDLgPRQLv7sSv8dYj4b1A8OYndP0Iw74";
+return $api;
+});
 function university_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
